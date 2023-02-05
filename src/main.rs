300e7b4798cc40fd88f7ed36d13e7561dc90eb41
kwannee <yleh1126@naver.com>
2023-02-06 02:14:51 +0900

    feat: 이름 받고 split

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let names = split_str(&input);
+    println!("{:?}", names);
+}
+
+fn split_str(str: &str) -> Vec<&str> {
+    str.split(' ').collect()
 }
