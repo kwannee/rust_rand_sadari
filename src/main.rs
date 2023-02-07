1e7675fdc76b2660d736cb756f8b163964ebbd13
kwannee <yleh1126@naver.com>
2023-02-08 01:46:49 +0900

    feat: 랜덤으로 이름 뽑기

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+use rand::prelude::*;
 use std::io;
 
 fn main() {
+    let names_input = get_input();
+    let names = split_str(&names_input);
+    let winner = pick_random_name(names);
+    println!("{}", winner);
+}
+
+fn get_input() -> String {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let names = split_str(&input);
-    println!("{:?}", names);
+    input
 }
 
 fn split_str(str: &str) -> Vec<&str> {
     str.split(' ').collect()
+}
+
+fn pick_random_name(names: Vec<&str>) -> &str {
+    names[gen_random_number(names.len() as f64)]
+}
+
+fn gen_random_number(limit: f64) -> usize {
+    let mut rng = thread_rng();
+    let random_number: f64 = rng.gen_range(0 as f64..limit);
+    random_number as usize
 }
